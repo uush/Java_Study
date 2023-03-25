76802d608eb29531eea164e01dcedbaee5724c73
uush <dbtmd447@gmail.com>
2023-03-25 22:39:49 +0900

    BAEKJOON Question Step1

---
+++ JavaPractice/src/pro18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pro18108 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		System.out.println(year-543);
+	}
+
+}
