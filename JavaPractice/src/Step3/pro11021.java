d0a44ee53ed04846e2cbdebae15844769ff3ec21
uush <dbtmd447@gmail.com>
2023-03-31 23:15:17 +0900

    BAEKJOON Step3 Problem Solving

--- JavaPractice/src/Step3/pro11021.java
+++ JavaPractice/src/Step3/pro11021.java
@@ -7,20 +7,16 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+public class pro11021 {
 
-public class pro15552 {
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
-		int T =  Integer.parseInt(bf.readLine());
-		
-		while(T>0) {
+		int T = Integer.parseInt(bf.readLine());
+		for(int i=1; i<=T; i++) {
 			st = new StringTokenizer(bf.readLine());
-			bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n"); //"\n"필요.
-			T--;
+			bw.write("Case #"+i+": "+(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
 		}
 		bw.close();
 	}
