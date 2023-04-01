c12820895ecfcefb860fbe5d1514d03fc1f8d7a8
uush <dbtmd447@gmail.com>
2023-04-02 00:31:10 +0900

    BAEKJOON Step3 Problem Solving

--- JavaPractice/src/Step3/pro10951.java
+++ JavaPractice/src/Step3/pro10951.java
@@ -7,20 +7,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class pro10952 {
+public class pro10951 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
-		while(true) {
-			st = new StringTokenizer(br.readLine());
+		String str =  "";
+		while((str = br.readLine())!=null) {
+			st = new StringTokenizer(str, " ");
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-			if(A==0 && B==0) {
-				break;
-			}
+			
 			System.out.println(A+B);
 		}
 		bw.close();
