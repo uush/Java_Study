fd05b9f550e7add0bb497fc7bd17825faf583ea3
uush <dbtmd447@gmail.com>
2023-04-01 23:50:10 +0900

    BAEJOON Step3 problem Solving

--- JavaPractice/src/Step3/pro10952.java
+++ JavaPractice/src/Step3/pro10952.java
@@ -1,10 +1,28 @@
 package Step3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class pro10952 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if(A==0 && B==0) {
+				break;
+			}
+			System.out.println(A+B);
+		}
+		bw.close();
 	}
-
 }
